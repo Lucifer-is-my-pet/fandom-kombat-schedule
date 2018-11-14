11b1293a4b9e97ba7ab114b9db936dcc5a102dee
Дарья Гуральник <6801611+Lucifer-is-my-pet@users.noreply.github.com>
2018-11-14 10:17:39 +0500

    обновлены года
    надо бы как-то это нормально реализовать...

--- discord_bot.py
+++ discord_bot.py
@@ -7,20 +7,20 @@ bot = commands.Bot(command_prefix="$", description="A bot that gives FK schedule
 
 TODAY = date.today()
 
-QUEST_SCHEDULE = {"визитка": [date(TODAY.year, 1, 19), date(TODAY.year, 1, 20)],
-                  "тексты нерейтинг": [date(TODAY.year, 1, 26), date(TODAY.year, 1, 27)],
+QUEST_SCHEDULE = {"визитка": [date(2019, 1, 19), date(2019, 1, 20)],
+                  "тексты нерейтинг": [date(2019, 1, 26), date(2019, 1, 27)],
                   #"драбблы нерейтинг": [date(TODAY.year, 1, 26), date(TODAY.year, 1, 27)],
                   #"мини нерейтинг": [date(TODAY.year, 1, 26), date(TODAY.year, 1, 27)],
-                  "визуал нерейтинг": [date(TODAY.year, 2, 1), date(TODAY.year, 2, 2)],
+                  "визуал нерейтинг": [date(2019, 2, 1), date(2019, 2, 2)],
                   #"миди нерейтинг": [date(TODAY.year, 1, 26), date(TODAY.year, 1, 27)],
-                  "челлендж": [date(TODAY.year, 2, 8), date(TODAY.year, 2, 9)],
+                  "челлендж": [date(2019, 2, 8), date(2019, 2, 9)],
                   #"бб": [date(TODAY.year, 8, 26), date(TODAY.year, 8, 27)],
-                  "тексты рейтинг": [date(TODAY.year, 2, 16), date(TODAY.year, 2, 17)],
+                  "тексты рейтинг": [date(2019, 2, 16), date(2019, 2, 17)],
                   #"драбблы рейтинг": [date(TODAY.year, 2, 16), date(TODAY.year, 2, 17)],
                   #"мини рейтинг": [date(TODAY.year, 2, 16), date(TODAY.year, 2, 17)],
-                  "визуал рейтинг": [date(TODAY.year, 2, 22), date(TODAY.year, 2, 23)],
+                  "визуал рейтинг": [date(2019, 2, 22), date(2019, 2, 23)],
                   #"миди рейтинг": [date(TODAY.year, 2, 16), date(TODAY.year, 2, 17)],
-                  "спецквест": [date(TODAY.year, 2, 28), date(TODAY.year, 3, 1)]}
+                  "спецквест": [date(2019, 2, 28), date(2019, 3, 1)]}
 
 '''LEVEL_SCHEDULE = {1: date(TODAY.year, 7, 21),
                   2: date(TODAY.year, 7, 26),
@@ -30,31 +30,31 @@ QUEST_SCHEDULE = {"визитка": [date(TODAY.year, 1, 19), date(TODAY.year, 1
                   6: date(TODAY.year, 9, 26)}'''
 
 VOTE_SCHEDULE = [
-    {"name": "визитка", "beginning": date(TODAY.year, 1, 21), "end": date(TODAY.year, 2, 3),
+    {"name": "визитка", "beginning": date(2019, 1, 21), "end": date(2019, 2, 3),
      "out": "визитку"},
-    {"name": "тексты нерейтинг", "beginning": date(TODAY.year, 2, 28), "end": date(TODAY.year, 2, 10),
+    {"name": "тексты нерейтинг", "beginning": date(2019, 2, 28), "end": date(2019, 2, 10),
      "out": "нерейтинговые тексты", "rating": False},
     #{"name": "драбблы нерейтинг", "beginning": date(TODAY.year, 7, 28), "end": date(TODAY.year, 8, 18),
      #"out": "нерейтинговые драбблы", "rating": False},
     #{"name": "мини нерейтинг", "beginning": date(TODAY.year, 8, 2), "end": date(TODAY.year, 8, 23),
      #"out": "нерейтинговые мини", "rating": False},
-    {"name": "визуал нерейтинг", "beginning": date(TODAY.year, 2, 3), "end": date(TODAY.year, 2, 16),
+    {"name": "визуал нерейтинг", "beginning": date(2019, 2, 3), "end": date(2019, 2, 16),
      "out": "нерейтинговый визуал", "rating": False},
     #{"name": "миди нерейтинг", "beginning": date(TODAY.year, 8, 12), "end": date(TODAY.year, 9, 2),
      #"out": "нерейтинговые миди", "rating": False},
-    {"name": "челлендж", "beginning": date(TODAY.year, 2, 10), "end": date(TODAY.year, 2, 23), "out": "челлендж"},
+    {"name": "челлендж", "beginning": date(2019, 2, 10), "end": date(2019, 2, 23), "out": "челлендж"},
     #{"name": "бб", "beginning": date(TODAY.year, 8, 28), "end": date(TODAY.year, 10, 19), "out": "ББ"},
-    {"name": "тексты рейтинг", "beginning": date(TODAY.year, 2, 18), "end": date(TODAY.year, 3, 3),
+    {"name": "тексты рейтинг", "beginning": date(2019, 2, 18), "end": date(2019, 3, 3),
      "out": "нерейтинговые драбблы", "rating": True},
     #{"name": "драбблы рейтинг", "beginning": date(TODAY.year, 9, 8), "end": date(TODAY.year, 9, 29),
      #"out": "рейтинговые драбблы", "rating": True},
     #{"name": "мини рейтинг", "beginning": date(TODAY.year, 9, 13), "end": date(TODAY.year, 10, 4),
      #"out": "рейтинговые мини", "rating": True},
-    {"name": "визуал рейтинг", "beginning": date(TODAY.year, 2, 24), "end": date(TODAY.year, 3, 9),
+    {"name": "визуал рейтинг", "beginning": date(2019, 2, 24), "end": date(2019, 3, 9),
      "out": "рейтинговый визуал", "rating": True},
     #{"name": "миди рейтинг", "beginning": date(TODAY.year, 9, 23), "end": date(TODAY.year, 10, 14),
      #"out": "рейтинговые миди", "rating": True},
-    {"name": "спецквест", "beginning": date(TODAY.year, 3, 2), "end": date(TODAY.year, 3, 15), "out": "спецквест"}
+    {"name": "спецквест", "beginning": date(2019, 3, 2), "end": date(2019, 3, 15), "out": "спецквест"}
 ]
 
 
